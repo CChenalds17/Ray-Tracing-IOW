e090a66b3f90ad0f73980e8fc88650fc5419e737
Charlie Chen <charlie.dd.chen@gmail.com>
2025-02-28 15:53:46 -0500

    6.1: Shading with Surface Normals

--- main.cc
+++ main.cc
@@ -6,18 +6,27 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    /* Returns parameter `t` corresopnding to when the ray hits the sphere */
+
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant) ) / (2.0*a);
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1)); // unit normal vector (at origin)
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1); // normalize each component to be between 0 and 1
     }
 
     vec3 unit_direction = unit_vector(r.direction());
